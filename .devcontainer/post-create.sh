e731c98dcae13bf584486d36d73a9d4b849f90a9
John Cole <jhcole@purdue.edu>
2024-05-06 00:14:30 +0000

    Clean up post-create.sh, and add copilot-chat.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,13 +1,8 @@
-sudo apt-get update && sudo apt-get install -y tesseract-ocr ranger
-
-alias r='. ranger'
-
-# Install Python dependencies
-pip3 install --upgrade pip
-# pip3 install --user -r requirements.txt
+sudo apt-get update && sudo apt-get install -y tesseract-ocr
 
 # Install pre-commit hooks
 pre-commit install
 
-# Install this package in editable mode
+# Install this package and its dependencies in editable mode
+pip3 install --upgrade pip
 pip3 install -e .[dev]
