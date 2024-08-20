2c2c96a72a4ace024c4b29abe6f5566d51eee5a7
Jack Scarfo <jackdscarfo@gmail.com>
2024-08-19 21:43:04 -0400

    bump version message

--- src/generic_grader/utils/importer.py
+++ src/generic_grader/utils/importer.py
@@ -70,6 +70,7 @@ class Importer:
             fail_msg += f"\files: {os.listdir()}"
             sym_links = [p.name for p in Path().iterdir() if p.is_symlink()]
             fail_msg += f"\nsym_links: {sym_links})"
+            fail_msg += "v1.1"
             test.failureException = type(e)
 
         # Fail outside of the except block
