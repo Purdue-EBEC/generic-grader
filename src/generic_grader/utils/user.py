c9f0908f4aecd1207123974973802cc2e1423b30
Jack Scarfo <jackdscarfo@gmail.com>
2024-09-20 11:48:15 -0400

    Try always creating the object

--- src/generic_grader/utils/user.py
+++ src/generic_grader/utils/user.py
@@ -346,6 +346,7 @@ class __User__:
                         patch(
                             *p.get("args", ()),  # permit missing args
                             **p.get("kwargs", {}),  # permit missing kwargs
+                            create=True,
                         )
                     )
 
