e1f49bbfe6faf35aee9f87a67849732e07dc013e
John Cole <jhcole@purdue.edu>
2024-09-23 04:48:16 -0400

    Add patches for pyplot show and savefig.

--- tests/utils/test_patches.py
+++ tests/utils/test_patches.py
@@ -2,9 +2,34 @@ import pytest
 
 from generic_grader.utils.options import Options
 from generic_grader.utils.patches import (
+    make_pyplot_noop_patches,
     make_turtle_done_patches,
     make_turtle_write_patches,
 )
+
+
+def test_make_pyplot_noop_patches_names():
+    result = make_pyplot_noop_patches(["sub_module"])
+
+    mpl_func_name_0, _ = result[0]["args"]
+    sub_func_name_0, _ = result[1]["args"]
+    mpl_func_name_1, _ = result[2]["args"]
+    sub_func_name_1, _ = result[3]["args"]
+
+    assert mpl_func_name_0 == "matplotlib.pyplot.savefig"
+    assert mpl_func_name_1 == "matplotlib.pyplot.show"
+    assert sub_func_name_0 == "sub_module.savefig"
+    assert sub_func_name_1 == "sub_module.show"
+
+    with pytest.raises(IndexError):
+        result[4]
+
+
+def test_make_pyplot_noop_patches_format():
+    """Make sure the patches are properly formatted and load into Options properly."""
+    result = make_pyplot_noop_patches(["sub_module"])
+
+    assert Options(patches=result)
 
 
 def test_make_turtle_write_patches_names():
